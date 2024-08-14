24a62b4a5c8982f749e646984300d031747e5a76
Shrey Mehta <79155940+Shrey0601@users.noreply.github.com>
2024-08-14 11:30:40 +0000

    Dataset updated

--- run.py
+++ run.py
@@ -36,10 +36,10 @@ import pandas as pd
 # df = data.load_dataset_adult()
 # # df = data.load_dataset_credit()  # Uncomment this line to use the credit dataset
 # df = data.load_dataset_lawschool()
-df = pd.read_csv("data/law_school_data_quantized.csv")
+df = data.load_dataset_lawschool()
 # df.dropna(inplace = True)
 LABEL_COLUMN = "gpa"  #" label" (for Adult); "default" (for Credit)
-FEATURE_NAMES = list(df.keys())
+FEATURE_NAMES = list(df.keys()) 
 FEATURE_NAMES.remove(LABEL_COLUMN)
 PROTECTED_COLUMNS = ['race1_asian', 'race1_black', 'race1_hisp', 'race1_other', 'race1_white'] # ['race_White', 'race_Black', 'race_Other_combined'] (for Adult); ['EDUCATION_grad', 'EDUCATION_uni', 'EDUCATION_hs_other'] (for Credit)
 
